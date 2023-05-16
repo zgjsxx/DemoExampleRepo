bc1674cb73ee0a14a4bc5760e8b1a3e309e3c053
zgjsxx <119160524@qq.com>
2023-05-16 17:45:31 +0800

    add new demo

---
+++ cmake/find_package_module_demo1/libadd.cc
@@ -0,0 +1,6 @@
+#include "libadd.h"
+ 
+int add(int i, int j)
+{
+	return i + j;
+}
---
+++ cmake/find_package_module_demo1/test/addtest.cc
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libadd.h"
+ 
+int main(int argc, char *argv[]) 
+{
+	printf("1 + 2 = %d\n", add(1, 2));
+	return 0;
+}
